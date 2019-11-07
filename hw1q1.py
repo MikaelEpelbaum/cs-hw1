df7fd18d6289dfb9c694f76d7ba67a98df605709
mikael epelbaum <mikaelepelbaum@gmail.com>
2019-11-07 08:48:48 +0200

    ex 1 done

--- hw1q1.py
+++ hw1q1.py
@@ -29,17 +29,14 @@ d_q = int(input())
 d_q_val = d_q >= 1
 
 # a_q + c_quantity has to be less than 5
-a_and_c_quantity = a_q * a_q_val + c_q * c_q_val + d_q * d_q_val
+a_and_c_quantity = a_q * a_q_val + c_q * c_q_val
 a_and_c_quantity_val = a_and_c_quantity <= 5
-print(a_and_c_quantity_val)
 
 total_purchase = round(a_p * a_p_val * a_q * a_q_val + b_p * b_p_val * b_q * b_q_val + c_p * c_p_val * c_q * c_q_val + d_p * d_p_val * d_q * d_q_val, 2)
 total_items = a_q * a_q_val + b_q * b_q_val + c_q * c_q_val + d_q * d_q_val
 average_item_price = round(total_purchase/total_items, 2)
 
-print('{} {} {}'.format(total_purchase, total_items, average_item_price))
-
 # in case one of the conditions isn't respected this error message should be displayed:
 total_validity = (a_p_val and a_q_val and b_p_val and b_q_val and c_p_val and c_q_val and d_p_val and d_q_val and a_and_c_quantity_val)
-print(total_validity)
-print(''.format(total_validity * "Invalid Purchase"))
+result = '{} {} {}'.format(total_purchase, total_items, average_item_price)
+print(result * total_validity or (total_validity or "Invalid Purchase"))
